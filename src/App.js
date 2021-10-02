1c4c1641f815056ae40dd26704487537febc3152
Michael <michael_parvis@hotmail.com>
2021-10-02 03:26:27 +0100

    refactored mapstatetoprops to use reselect on all necessary components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import "./App.css";
 
@@ -12,6 +13,7 @@ import SigninSignup from "./components/signin-signup/SigninSignup";
 import { auth, createUserProfileDocument } from "./firebase/firebase-utils";
 
 import { setCurrentUser } from "./redux/user/user-actions";
+import { selectCurrentUser } from "./redux/user/user-selectors";
 
 class App extends Component {
   componentDidMount() {
@@ -57,8 +59,8 @@ class App extends Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
